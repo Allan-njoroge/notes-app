c9e468a640119e269687bb1e54a528981dabea5d
Allan-njoroge <allandavis254@gmail.com>
2024-06-17 21:10:35 +0300

    notes

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Note(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.TextField()
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('notes/<str:user_id>/', views.get_notes, name="notes"),
-    path('note/<str:pk/', views.get_note, name="note"),
+    path('note/<str:pk>/', views.get_note, name="note"),
     path('create/', views.create_note, name="create"),
     path('update/<str:pk>/', views.update_note, name="update"),
     path('delete/<str:pk>/', views.delete_note, name="delete"),
