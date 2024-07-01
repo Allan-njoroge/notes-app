be0cfe7625d64158acd0db16a66877805b9713d2
Allan-njoroge <allandavis254@gmail.com>
2024-07-01 20:12:10 +0300

    urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('register/', views.Register, name="notes"),
-    path('login/', views.Login, name="note"),
+    path('register', views.Register, name="notes"),
+    path('login', views.Login, name="note"),
     path('logout', views.Logout, name="create"),
+    path('<int:id>/', views.GetUser, name="getUser")
 ]
