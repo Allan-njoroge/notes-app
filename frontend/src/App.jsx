57bb2dd8afa89e7363a77ae0d7591197d595cf14
Allan-njoroge <allandavis254@gmail.com>
2024-07-02 21:58:44 +0300

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Newnote from "./pages/Newnote";
+import Note from "./pages/Note";
+import NotFound from "./pages/NotFound";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { currentUser } = useContext(AuthContext)
+  console.log(currentUser)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Unprotected routes */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+
+        {/* Protected routes */}
+        <Route path="/" element={ <PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/new" element={ <PrivateRoute><Newnote /></PrivateRoute> } />
+        <Route path="/note" element={ <PrivateRoute><Note /></PrivateRoute> } />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
